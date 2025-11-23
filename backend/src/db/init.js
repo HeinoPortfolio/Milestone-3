e49f556a580cff853a761cb9fc27b8ab903b77a5
HeinoPorfolio <meheino@sbcglobal.net>
2025-11-23 01:10:58 -0500

    chore: setup env variables

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -2,8 +2,7 @@
 import mongoose from 'mongoose'
 
 export function initDatabase() {
-  const DATABASE_URL = 'mongodb://localhost:27017/recipe-blog-v2'
-  //const DATABASE_URL = process.env.DATABASE_URL
+  const DATABASE_URL = process.env.DATABASE_URL
 
   mongoose.connection.on('open', () => {
     console.info('Successfully connected to the database: ', DATABASE_URL)
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,21 @@
 import { app } from './app.js'
+import { initDatabase } from './db/init.js'
+import dotenv from 'dotenv'
 
-// Port for the database ======================================================
-const PORT = 3000
+// Function to setup the environment variables ================================
+dotenv.config()
 
-app.listen(PORT)
+// Initialize the database ====================================================
+try {
+  await initDatabase()
 
-console.info(`Express Server running on: http://localhost:${PORT}`)
+  // Get the database port number =============================================
+  const PORT = process.env.PORT
+
+  app.listen(PORT)
+
+  // Display message to terminal about success
+  console.info(`Express Server running on: http://localhost:${PORT}`)
+} catch (err) {
+  console.error('Error connecting to database: ', err)
+}
