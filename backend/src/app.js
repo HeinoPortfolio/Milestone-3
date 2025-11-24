a23e24cdfe6fd3ad3f662990c5d0f10f190d064d
HeinoPorfolio <meheino@sbcglobal.net>
2025-11-24 16:01:13 -0500

    chore: setup socketio backend and installed packages

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,32 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
 // Import the routes ======================================
 import { recipesRoutes } from './routes/recipes.js'
 import { userRoutes } from './routes/users.js'
 
+// Socket.io imports ======================================
+import { createServer } from 'node:http'
+import { Server as SocketIOServer } from 'socket.io'
+
 const app = express()
+
+// Creates a new node server ==============================
+const server = createServer(app)
+
+// Create a new Socket.io server =========================
+const io = new SocketIOServer(server, {
+  cors: { origin: '*', methods: ['GET', 'POST'] },
+})
+
+// Use the socket to listen to the connections from the
+// clients
+io.on('connection', (socket) => {
+  console.log('User connected: ', socket.id)
+  socket.on('disconnect', () => {
+    console.log('User disconnected: ', socket.id)
+  })
+})
 
 // Use the body parser ====================================
 app.use(bodyParser.json())
@@ -24,4 +44,4 @@ app.get('/', (req, res) => {
 })
 
 // Export the app
-export { app }
+export { server as app }
